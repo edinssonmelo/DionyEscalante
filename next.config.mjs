ba8318aace95e11e6bd232827d13dbbb2b4c5495
Mackenzie <Mackenzie-98>
2025-11-17 23:00:07 -0500

    Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  // Si tu repositorio NO se llama "username.github.io", descomenta y ajusta el basePath
+  // basePath: '/LandingRMP', // Reemplaza 'LandingRMP' con el nombre de tu repositorio
+  // trailingSlash: true,
 }
 
 export default nextConfig
